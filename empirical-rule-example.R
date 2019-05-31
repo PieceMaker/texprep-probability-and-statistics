a828faebad45e9e57983f57f464f0e5f7d606b26
Jonathan Adams <jd.adams16@gmail.com>
2019-05-30 22:41:40 -0500

    Added function to visually demonstrate empirical rule.

---
+++ empirical-rule-example.R
@@ -0,0 +1,42 @@
+library(ggplot2)
+
+plotEmpiricalRule <- function(mu, sigma, n = 100, N = 1000000) {
+    population <- rnorm(n = N, mean = mu, sd = sigma)
+    mu <- mean(population)
+    sigma <- sd(population)
+    ySample <- sample(x = population, replace = F, size = n)
+    ybar <- mean(ySample)
+    s <- sd(ySample)
+    
+    y <- rbind(
+        data.frame(type = "Population", y = population),
+        data.frame(type = "Sample", y = ySample)
+    )
+    
+    g <- ggplot(data = y, aes(x = y, fill = type)) +
+        geom_histogram(aes(y=..density..), alpha=0.2, position="identity", color = '#737373') +
+        geom_density(alpha = 0.3, color = '#737373') +
+        facet_grid(rows = vars(type)) +
+        geom_vline(xintercept = mu, color = '#F8766D', linetype = 'dashed', size = 0.5) +
+        geom_vline(xintercept = ybar, color = '#00BFC4', linetype = 'dashed', size = 0.5) +
+        geom_vline(xintercept = ybar - s, color = '#2A6637', size = 1) +
+        geom_vline(xintercept = ybar + s, color = '#2A6637', size = 1) +
+        geom_vline(xintercept = ybar - 2*s, color = '#36C488', size = 1) +
+        geom_vline(xintercept = ybar + 2*s, color = '#36C488', size = 1) +
+        geom_vline(xintercept = ybar - 3*s, color = '#7BE19A', size = 1) +
+        geom_vline(xintercept = ybar + 3*s, color = '#7BE19A', size = 1) +
+        labs(
+            title = sprintf(
+                '\U003BC: %s, \U0233: %s, \u03C3: %s, s: %s',
+                round(mu, digits = 2),
+                round(ybar, digits = 2),
+                round(sigma, digits = 2),
+                round(s, digits = 2)
+            )
+        ) +
+        theme(plot.title = element_text(hjust = 0.5))
+    
+    g
+}
+
+plotEmpiricalRule(100, 10, n = 1000)
